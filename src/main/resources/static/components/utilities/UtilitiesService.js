38c76c63df58c091bd4c8eb02ba9e5d35173dc5d
Akshat Singhal <akshat.singhal2016@vitstudent.ac.in>
2020-10-14 09:47:34 +0530

    Removed properties

--- src/main/resources/static/components/utilities/UtilitiesService.js
+++ src/main/resources/static/components/utilities/UtilitiesService.js
@@ -2,35 +2,35 @@ var UtilitiesService = function(HttpRequestService){
     var service = function(){}
 
     service.prototype.listAllMakes = function(){
-        return HttpRequestService.executeGetRequest("http://localhost:8080/api/utilities/getallmakes");
+        return HttpRequestService.executeGetRequest("/api/utilities/getallmakes");
     }
 
     service.prototype.addNewMake = function(json){
-        return HttpRequestService.executeJsonPostRequest("http://localhost:8080/api/utilities/insertmake",json);
+        return HttpRequestService.executeJsonPostRequest("/api/utilities/insertmake",json);
     }
 
     service.prototype.listAllCounterParties = function(){
-        return HttpRequestService.executeGetRequest("http://localhost:8080/api/utilities/getallcounterparties"); 
+        return HttpRequestService.executeGetRequest("/api/utilities/getallcounterparties"); 
     }
 
     service.prototype.addNewCounterParty = function(json){
-        return HttpRequestService.executeJsonPostRequest("http://localhost:8080/api/utilities/insertcounterparty",json);
+        return HttpRequestService.executeJsonPostRequest("/api/utilities/insertcounterparty",json);
     }
 
     service.prototype.listAllMaterials = function(){
-        return HttpRequestService.executeGetRequest("http://localhost:8080/api/utilities/getallmaterials");
+        return HttpRequestService.executeGetRequest("/api/utilities/getallmaterials");
     }
 
     service.prototype.addNewMaterial = function(json){
-        return HttpRequestService.executeJsonPostRequest("http://localhost:8080/api/utilities/insertmaterial",json);
+        return HttpRequestService.executeJsonPostRequest("/api/utilities/insertmaterial",json);
     }
 
     service.prototype.listAllQuantities = function(){
-        return HttpRequestService.executeGetRequest("http://localhost:8080/api/utilities/getallquantities");
+        return HttpRequestService.executeGetRequest("/api/utilities/getallquantities");
     }
 
     service.prototype.addNewQuantity = function(json){
-        return HttpRequestService.executeJsonPostRequest("http://localhost:8080/api/utilities/insertquantity",json);
+        return HttpRequestService.executeJsonPostRequest("/api/utilities/insertquantity",json);
     }
 
     return new service();
