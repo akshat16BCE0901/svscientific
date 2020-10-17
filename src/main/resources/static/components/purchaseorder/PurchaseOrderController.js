18ba1460c08a7fa0b0b3856af73bc6cd5b586c09
Akshat Singhal <akshat.singhal2016@vitstudent.ac.in>
2020-10-17 19:51:29 +0530

    Added Purchase ORder UI

--- src/main/resources/static/components/purchaseorder/PurchaseOrderController.js
+++ src/main/resources/static/components/purchaseorder/PurchaseOrderController.js
@@ -9,48 +9,9 @@ var PurchaseOrderController = function($scope,PurchaseOrderService,UtilitiesServ
     $scope.allQuantities = [];
     $scope.temporaryOrders = [];
 
-    $scope.addRowToTempTable = function(obj){
-        $scope.temporaryOrders.push(obj);
-    }
-
-    $scope.removeSelectedRows = function(){
-        console.log("Function called removeSelectedRows");
-        console.log("Temporary array is "+$scope.temporaryOrders);
-        var newarr = [];
-        $scope.temporaryOrders.forEach(function (value) {
-            console.log("Value is "+value);
-            if (!value.Remove) {
-                newarr.push(value);
-            }
-        });
-        console.log("New arr is "+newarr);
-        $scope.temporaryOrders = newarr;
-    }
 
     $scope = displayData = function(){
         console.log($scope.temporaryOrders);
-    }
-    $scope.addRow = function(){
-        var objToAdd = {
-            "material" : {
-                "id" : $scope.materialName
-            },
-            "make" : {
-                "id" : $scope.makeName
-            },
-            "numitems" : $scope.numitems,
-            "quantity" : {
-                "id" : $scope.quantity
-            },
-            "catid": $scope.catid,
-            "rate" : $scope.rate,
-            "per" : $scope.per,
-            "disc" : $scope.disc,
-            "hsncode" : $scope.hsncode,
-            "gst" : $scope.gst
-        };
-        console.log("Entered model is ====> "+JSON.stringify(objToAdd));
-        $scope.addRowToTempTable(objToAdd);
     }
 
     $scope.listAllMakes = function(){
