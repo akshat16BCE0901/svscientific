cb83bf94c3b8cb7a2aacc3d3092765a72b070789
Akshat Singhal <akshat.singhal2016@vitstudent.ac.in>
2020-10-17 11:54:36 +0530

    Added Purchase ORder UI

--- src/main/java/com/svscientific/datamanagement/controllers/PurchaseOrderController.java
+++ src/main/java/com/svscientific/datamanagement/controllers/PurchaseOrderController.java
@@ -5,6 +5,7 @@ import com.svscientific.datamanagement.services.PurchaseOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,7 +14,7 @@ public class PurchaseOrderController {
 
     @Autowired private PurchaseOrderService purchaseOrderService;
 
-    @RequestMapping("/saveorder")
+    @RequestMapping(value = "/saveorder", method = RequestMethod.POST)
     public void saveOrder(@RequestBody Orders order){
         purchaseOrderService.addNewOrder(order);
     }
